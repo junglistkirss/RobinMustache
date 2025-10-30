a86ff4d95ebe646e54425e3807b3b9847c5c3399
junglistkirss <junglist.kirss@gmail.com>
2025-10-30 23:36:37 +0100

    Clean comments

--- Robin/Nodes/INode.cs
+++ Robin/Nodes/INode.cs
@@ -1,8 +1,5 @@
 namespace Robin.Nodes;
 
-// Reuse previous TokenType and Token
-
-// AST nodes
 public interface INode
 {
     TOut Accept<TOut, TArgs>(INodeVisitor<TOut, TArgs> visitor, TArgs args);
