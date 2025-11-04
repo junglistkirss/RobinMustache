1d6183bf0a885bf89c3ed6cc160948fe110bbae1
CBO <junglist.kirss@gmail.com>
2025-11-04 10:32:13 +0000

    Configure benchmarks

--- Robin.Benchmarks/TweetsBencnmarks.cs
+++ Robin.Benchmarks/TweetsBencnmarks.cs
@@ -1,22 +1,69 @@
 ﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Jobs;
 using Microsoft.Extensions.DependencyInjection;
-using Perfolizer;
-using Robin.Abstractions;
 using Robin.Abstractions.Extensions;
 using Robin.Contracts.Nodes;
-using System;
-using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
 using System.Text.Json;
-using System.Threading.Tasks;
 
 namespace Robin.Benchmarks;
 
+/// <summary>
+/// Configuration automatique : choisit Fast ou Debug selon une variable d'environnement ou un argument CLI.
+/// </summary>
+public class AutoBenchmarkConfig : ManualConfig
+{
+    private const string DEBUG_MODE = "Debug";
+    private const string FAST_MODE = "Fast";
+    private const string ENV_MODE = "BENCH_MODE";
 
+    public AutoBenchmarkConfig()
+    {
+        // Vérifie d'abord une variable d'environnement (par ex. BENCH_MODE=Debug)
+        string? mode = Environment.GetEnvironmentVariable(ENV_MODE);
+
+        // Sinon, vérifie les arguments de ligne de commande
+        string[] args = Environment.GetCommandLineArgs();
+        if (mode is null && args.Length > 1)
+        {
+            foreach (string arg in args)
+            {
+                if (arg.Equals("--debug", StringComparison.OrdinalIgnoreCase))
+                    mode = DEBUG_MODE;
+                else if (arg.Equals("--fast", StringComparison.OrdinalIgnoreCase))
+                    mode = FAST_MODE;
+            }
+        }
+
+        // Choisit la configuration adaptée
+        if (string.Equals(mode, DEBUG_MODE, StringComparison.OrdinalIgnoreCase))
+        {
+            AddJob(Job.Dry
+                .WithWarmupCount(1)
+                .WithIterationCount(3)
+                .WithLaunchCount(1)
+                .WithId(DEBUG_MODE));
+        }
+        else
+        {
+            AddJob(Job.Default
+                .WithWarmupCount(8)
+                .WithIterationCount(32)
+                .WithLaunchCount(1)
+                .WithId(FAST_MODE));
+        }
+
+        // AddDiagnoser(MemoryDiagnoser.Default);
+        // AddExporter(MarkdownExporter.GitHub);
+    }
+}
+
+[Config(typeof(AutoBenchmarkConfig))]
 [MemoryDiagnoser]
+[DisassemblyDiagnoser]
 [MarkdownExporter]
 public class TweetsBencnmarks
 {
@@ -26,8 +73,6 @@ public class TweetsBencnmarks
     private readonly IStringRenderer renderer;
     public TweetsBencnmarks()
     {
-
-
         ServiceCollection services = [];
         services
             .AddServiceEvaluator()
