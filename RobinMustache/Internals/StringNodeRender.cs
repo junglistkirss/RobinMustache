47785f10e9040ed78ed6cd43ac9544e7fcdbeb17
junglistkirss <junglist.kirss@gmail.com>
2025-11-15 02:05:13 +0100

    Fix Trailing on section considre"d as collection

--- RobinMustache/Internals/StringNodeRender.cs
+++ RobinMustache/Internals/StringNodeRender.cs
@@ -57,10 +57,26 @@ internal sealed class StringNodeRender(IEnumerable<IPartialLoader> loaders) : IN
         bool shouldRenderTree = (!node.Inverted && thruly) || (node.Inverted && !thruly);
         if (shouldRenderTree)
         {
-            RenderTree(context, value, facade, node.Children.AsSpan());
+            if (facade.IsCollection(value, out IIterator? iterator) && iterator is not null)
+            {
+                if (node.TrailingBreak is not null && ((node.Inverted && shouldRenderTree) || (!node.Inverted && shouldRenderTree)))
+                    iterator.Iterate(value, context, [.. node.Children.AsSpan(), node.TrailingBreak], this);
+                else
+                    iterator.Iterate(value, context, node.Children.AsSpan(), this);
+            }
+            else
+            {
+                using (DataContext.Push(value))
+                {
+                    foreach (INode child in node.Children.AsSpan())
+                    {
+                        child.Accept(this, context);
+                    }
+                }
+                if (node.TrailingBreak is not null && ((node.Inverted && shouldRenderTree) || (!node.Inverted && shouldRenderTree)))
+                    VisitLineBreak(node.TrailingBreak, context);
+            }
         }
-        if (node.TrailingBreak is not null && ((node.Inverted && shouldRenderTree) || (!node.Inverted && shouldRenderTree)))
-            VisitLineBreak(node.TrailingBreak, context);
     }
 
     public void VisitPartialCall(PartialCallNode node, RenderContext<StringBuilder> context)
@@ -78,26 +94,18 @@ internal sealed class StringNodeRender(IEnumerable<IPartialLoader> loaders) : IN
                     ReadOnlyDictionary<string, ImmutableArray<INode>> tempPartials = new(span.ExtractsPartials(context.Partials));
                     using (new PartialsScope<StringBuilder>(context, tempPartials))
                     {
-                        RenderTree(context, value, facade, span);
+                        using (DataContext.Push(value))
+                        {
+                            foreach (INode child in span)
+                            {
+                                child.Accept(this, context);
+                            }
+                        }
                     }
                     return;
                 }
             }
         }
-    }
-
-    private void RenderTree(RenderContext<StringBuilder> context, object? value, IDataFacade facade, ReadOnlySpan<INode> partialTemplate)
-    {
-        if (facade.IsCollection(value, out IIterator? iterator) && iterator is not null)
-            iterator.Iterate(value, context, partialTemplate, this);
-        else
-            using (DataContext.Push(value))
-            {
-                foreach (INode node in partialTemplate)
-                {
-                    node.Accept(this, context);
-                }
-            }
     }
 
     public void VisitLineBreak(LineBreakNode node, RenderContext<StringBuilder> context)
